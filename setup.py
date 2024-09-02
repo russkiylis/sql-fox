15d39c89f0eb48e62a2f2da018296b5b8ec4818e
russkiylis <diaminerr@yandex.ru>
2024-09-02 23:14:20 +0300

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sql-fox',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         "pymysql>=1.0.2",
